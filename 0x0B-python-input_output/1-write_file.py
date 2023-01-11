1563de46802bbe6dcdb8909463593e5d4b02d9e9
Eunice Oke <euniceoke2022@gmail.com>
2023-01-11 13:10:32 +0100

    0x0B

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,7 +5,8 @@ function that writes a string to a text file (UTF8) and returns the number of ch
 
 
 def write_file(filename="", text=""):
-    """ module write_file
     """
-    with open(filename, 'w') as f:
+    module write_file
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
         return f.write(text)
