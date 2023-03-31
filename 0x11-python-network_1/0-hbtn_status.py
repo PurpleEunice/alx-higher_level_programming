acd318646eef93ee4ab218a0c85be3dfde44bd12
Eunice Oke <euniceoke2022@gmail.com>
2023-03-31 23:42:03 +0100

    0. Updated

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-""" Script that fetches https://intranet.hbtn.io/status
-with the usage of the package urllib"""
+"""Fetches https://alx-intranet.hbtn.io/status."""
+import urllib.request
+
 
 if __name__ == "__main__":
-    import urllib.request
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as r:
-        html = r.read()
-        print('Body response:')
-        print('\t- type: {}' .format(type(html)))
-        print('\t- content: {}' .format(html))
-        print('\t- utf8 content: {}' .format(html.decode('utf-8')))
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
