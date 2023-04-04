695850c4c4db6d3709499e43a8fde717a8640e3d
Eunice Oke <euniceoke2022@gmail.com>
2023-04-04 21:10:27 +0100

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # Get the byte size of the HTTP response header for a given URL.
-curl -sI "$1"
-grep -i Content-Length
-awk '{print $2}'
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
